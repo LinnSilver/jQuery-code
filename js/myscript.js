aeabd3e9bcc0a3603e1b98ca7d7d99c1667b7f00
LinnSilver <baralinn@gmail.com>
2023-03-10 09:24:37 +0000

    Content

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-    
+    //$("h2").css("text-decoration", "underline"); //underlines all h2 
+    $("h2").addClass("underline"); //underlines all h2 
+    //$("ul").css("border", "solid 1px #ccc"); //add a border to all lists
+    $("ul").addClass("border"); //add a border to all lists
 });
